9dbad2a646d739aef2aa854599e89f156eaacd19
MydrieDybiStudios <abeme5254@gmail.com>
2026-02-12 21:51:05 +0500

    Add files via upload

---
+++ client/tailwind.config.js
@@ -0,0 +1,20 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        tgDark: '#0e1621',
+        tgPanel: '#17212b',
+        tgBlue: '#5288c1',
+        tgMsgIn: '#182533',
+        tgMsgOut: '#2b5278',
+        tgText: '#f5f5f5',
+        tgHint: '#6c7883'
+      }
+    },
+  },
+  plugins: [],
+}
